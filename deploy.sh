7c938ab8e5e5c9f0d94812c93e9e37f8ccf2301f
橙汁坤 <36911813+czkm@users.noreply.github.com>
2020-07-02 23:38:05 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,14 @@ npm run build
 # echo 'www.example.com' > CNAME
 
 git init
-git add -A
+git add .
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git pull origin master
-git push -u origin master
+git pull
+git push 
 cd -
 
